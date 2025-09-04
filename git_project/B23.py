8d8fe9ad3a69b0653bdd7e588f99fc6d2a81e4b1
steven <1832277031@qq.com>
2025-09-04 16:40:27 +0800

    Initial commit

---
+++ git_project/B23.py
@@ -0,0 +1,32 @@
+a = 8
+b = 3
+if (a<b) or (a>5):
+    E = True
+    print(E)
+else:
+    E = False
+    print(E)
+if  not (a>=b):
+    E = True
+    print(E)
+else:
+    E = False
+    print(E)
+if (a<8) and (b>3):
+    E = True
+    print(E)
+else:
+    E = False
+    print(E)
+if (a==8):
+    E = True
+    print(E)
+else:
+    E = False
+    print(E)
+if not (a==b) or (a>b):
+    E = True
+    print(E)
+else:
+    E = False
+    print(E)
